1e9eedff2f4f7da895b5699c23c922ca1743eff6
G4briel25 <warmerishoth@gmail.com>
2025-03-10 21:34:30 -0400

    tela de login criado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
-import './assets/main.css'
+import 'primeicons/primeicons.css';
+import './assets/style.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import { ConfirmationService } from 'primevue';
+import { DialogService } from 'primevue';
+import { ToastService } from 'primevue';
 
-createApp(App).mount('#app')
+
+import App from './App.vue'
+import AppState from "./plugins/appState.js";
+import ThemeSwitcher from "./components/ThemeSwitcher.vue";
+import Noir from "./presets/Noir.js"
+// import router from './router'
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+// app.use(router)
+app.use(PrimeVue, {
+  theme: {
+      preset: Noir,
+      options: {
+        prefix: 'p',
+        darkModeSelector: '.p-dark',
+        cssLayer: false,
+    }
+  }
+});
+
+app.use(AppState);
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+
+app.component('ThemeSwitcher', ThemeSwitcher);
+
+
+app.mount("#app");
