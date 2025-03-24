9527ac8eb24c914a90fd90e09b3811ee532d5918
G4briel25 <warmerishoth@gmail.com>
2025-03-24 18:41:33 -0400

    copyright adicionado em Appvue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
-<script setup>
-
-</script>
-
 <template>
-    <div style="height: 95vh">
+    <div style="min-height: 95vh;">
         <router-view></router-view>
     </div>
     <div class="copyright">Copyright by Gabriel</div>
@@ -11,7 +7,12 @@
 
 <style scoped>
 .copyright {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
     text-align: center;
+    padding: 10px 0;
 }
 
 @media (max-width: 996px) {
