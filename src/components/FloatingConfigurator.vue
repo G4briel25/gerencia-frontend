528e77a404f04636f80407e7050a2d6b93fe644c
G4briel25 <warmerishoth@gmail.com>
2025-03-29 12:34:06 -0400

    ajuste em modo dark

--- src/components/FloatingConfigurator.vue
+++ src/components/FloatingConfigurator.vue
@@ -1,13 +1,22 @@
 <script setup>
 import Button from 'primevue/button';
-import { computed } from 'vue';
+import {computed, onMounted} from 'vue';
 
 const toggleDarkMode = () => {
-    document.documentElement.classList.toggle('app-dark');
+    const isDark = document.documentElement.classList.toggle('app-dark');
+    localStorage.setItem('darkMode', isDark); // Salva a preferência no localStorage
 };
 
 const isDarkTheme = computed(() => {
     return document.documentElement.classList.contains('app-dark');
+});
+
+onMounted(() => {
+    // Verifica se a preferência de tema está salva no localStorage
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode === 'true') {
+        document.documentElement.classList.add('app-dark');
+    }
 });
 
 </script>
